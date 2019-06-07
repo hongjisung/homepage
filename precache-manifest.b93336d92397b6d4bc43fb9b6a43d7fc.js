a202bbb7e98e2ff0acd7ed4ca5bee1f4fa6a45dd
jisung hong <rhfktj@gmail.com>
2019-06-07 16:45:55 +0900

    Updates

---
+++ precache-manifest.b93336d92397b6d4bc43fb9b6a43d7fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212c94a519559c0947a88b54d6167d6f",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "a88d6aed6250dc293136",
+    "url": "/homepage/static/css/main.97a2fd51.chunk.css"
+  },
+  {
+    "revision": "1b702581d782f090fec7",
+    "url": "/homepage/static/js/2.927c5b8d.chunk.js"
+  },
+  {
+    "revision": "a88d6aed6250dc293136",
+    "url": "/homepage/static/js/main.c6eeb146.chunk.js"
+  },
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "1c686847d8d2b41c283e4615791537ed",
+    "url": "/homepage/static/media/home.1c686847.md"
+  },
+  {
+    "revision": "b6935ecbcb727bdc821ddde7e7dfb706",
+    "url": "/homepage/static/media/projects.b6935ecb.md"
+  },
+  {
+    "revision": "4e9ae6c3f1bb2201c20122fecd62e065",
+    "url": "/homepage/static/media/study.4e9ae6c3.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69d699ee19f3249e75874c3af071c952.js"
+  "/homepage/precache-manifest.b93336d92397b6d4bc43fb9b6a43d7fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
